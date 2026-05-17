37519662e82d84005a0b0284f0d50eefa788b2ff
Visal Vijay <150381094+B2krobbery@users.noreply.github.com>
2026-05-17 11:49:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,73 @@
 function updateDistrict(
-    name,
-    risk,
-    info,
-    shelters,
-    helpline
-) {
+name,
+risk,
+info,
+shelters,
+helpline
+){
 
-    document.getElementById("districtName").innerText = name;
-    document.getElementById("riskLevel").innerText = risk;
-    document.getElementById("safetyInfo").innerText = info;
-    document.getElementById("helpline").innerText = helpline;
+document.getElementById("districtName").innerText = name;
+document.getElementById("riskLevel").innerText = risk;
+document.getElementById("safetyInfo").innerText = info;
+document.getElementById("helpline").innerText = helpline;
 
-    const shelterContainer =
-        document.getElementById("shelters");
+const shelterContainer =
+document.getElementById("shelters");
 
-    shelterContainer.innerHTML = "";
+shelterContainer.innerHTML = "";
 
-    shelters.forEach(function(shelter) {
+shelters.forEach(function(shelter){
 
-        const p = document.createElement("p");
+const p = document.createElement("p");
 
-        p.innerText = shelter;
+p.innerText = shelter;
 
-        shelterContainer.appendChild(p);
+shelterContainer.appendChild(p);
 
-    });
-
-}
-
-function showGuide() {
-
-    alert(
-        "KERALA FLOOD EMERGENCY PREPAREDNESS GUIDE\n\n" +
-
-        "1. Store clean drinking water\n\n" +
-
-        "2. Keep emergency food supplies\n\n" +
-
-        "3. Charge phones and power banks\n\n" +
-
-        "4. Save emergency contact numbers\n\n" +
-
-        "5. Locate nearest relief shelter\n\n" +
-
-        "6. Keep a first aid kit ready\n\n" +
-
-        "7. Follow Kerala flood alerts"
-    );
+});
 
 }
 
-function showHelplines() {
+function showGuide(){
 
-    alert(
-        "KERALA EMERGENCY HELPLINE NUMBERS\n\n" +
+alert(
+"KERALA FLOOD EMERGENCY PREPAREDNESS GUIDE\n\n" +
 
-        "State Emergency: 1077\n\n" +
+"1. Store clean drinking water\n\n" +
 
-        "Disaster Management: 1070\n\n" +
+"2. Keep emergency food supplies\n\n" +
 
-        "Police: 100 / 112\n\n" +
+"3. Charge phones and power banks\n\n" +
 
-        "Ambulance: 108\n\n" +
+"4. Save emergency contact numbers\n\n" +
 
-        "Fire & Rescue: 101\n\n" +
+"5. Locate nearest relief shelter\n\n" +
 
-        "Women Helpline: 1091\n\n" +
+"6. Keep a first aid kit ready\n\n" +
 
-        "Child Helpline: 1098"
-    );
+"7. Follow Kerala flood alerts"
+);
+
+}
+
+function showHelplines(){
+
+alert(
+"KERALA EMERGENCY HELPLINE NUMBERS\n\n" +
+
+"State Emergency: 1077\n\n" +
+
+"Disaster Management: 1070\n\n" +
+
+"Police: 100 / 112\n\n" +
+
+"Ambulance: 108\n\n" +
+
+"Fire & Rescue: 101\n\n" +
+
+"Women Helpline: 1091\n\n" +
+
+"Child Helpline: 1098"
+);
 
 }
